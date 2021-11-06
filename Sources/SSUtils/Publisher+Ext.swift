9001036ff4b74792e28bc628805d6a20cfe4ad16
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-11-06 12:25:21 +0100

    Publisher asyncMap

---
+++ Sources/SSUtils/Publisher+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  Publisher+Ext.swift
+//  
+//
+//  Created by Sebastian Staszczyk on 06/11/2021.
+//
+
+import Combine
+
+public extension Publisher {
+    func asyncMap<T>(
+        _ transform: @escaping (Output) async -> T
+    ) -> Publishers.FlatMap<AnyPublisher<T, Never>, Self> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    let output = await transform(value)
+                    promise(.success(output))
+                }
+            }
+            .eraseToAnyPublisher()
+        }
+    }
+}
