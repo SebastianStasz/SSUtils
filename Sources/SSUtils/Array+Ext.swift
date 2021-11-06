ee60a34a92c87ac9c5ac209c604cab5d60406de1
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-11-06 12:25:12 +0100

    Array isNotEmpty

---
+++ Sources/SSUtils/Array+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  Array+Ext.swift
+//  SSUtils
+//
+//  Created by Sebastian Staszczyk on 06/11/2021.
+//
+
+import Foundation
+
+public extension Array {
+
+    var isNotEmpty: Bool {
+        !self.isEmpty
+    }
+}
