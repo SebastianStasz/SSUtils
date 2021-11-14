c30a94ad3fd1567cc52c4c12475ecf9ac053b9b1
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-11-14 18:52:02 +0100

    NSManagedObject extensions

---
+++ Sources/SSUtils/NSManagedObject+Ext.swift
@@ -0,0 +1,39 @@
+//
+//  NSManagedObject+Ext.swift
+//  SSUtils
+//
+//  Created by Sebastian Staszczyk on 14/11/2021.
+//
+
+import CoreData
+
+public extension NSManagedObject {
+
+    /// Returns a description of search criteria used to retrieve data from a persistent store.
+    /// - Parameters:
+    ///   - sortDescriptors: The sort descriptors of the fetch request.
+    ///   - predicate: The predicate of the fetch request.
+    /// - Returns: A description of search criteria as `NSFetchRequest`.
+    static func nsFetchRequest<T: NSManagedObject>(sortDescriptors: [NSSortDescriptor] = [], predicate: NSPredicate? = nil) -> NSFetchRequest<T> {
+        let name = String(describing: type(of: T.self)).replacingOccurrences(of: ".Type", with: "")
+        let request = NSFetchRequest<T>(entityName: name)
+        request.sortDescriptors = sortDescriptors
+        request.predicate = predicate
+        return request
+    }
+
+    /// Returns the managed object context with which the managed object is registered, if found.
+    func getContext() -> NSManagedObjectContext? {
+        guard let context = self.managedObjectContext else {
+            assertionFailure("Context coulnd not be found in: \(self.description)")
+            return nil
+        }
+        return context
+    }
+
+    /// Specifies an object that should be removed from its persistent store when changes are committed.
+    func delete() {
+        guard let context = getContext() else { return }
+        context.delete(self)
+    }
+}
