f0bd55a81761d34afb4a16a8c574ac13fb19baf3
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-11-29 17:28:54 +0100

    Infinite width

--- Sources/SSUtils/Extensions/View+Ext.swift
+++ Sources/SSUtils/Extensions/View+Ext.swift
@@ -15,4 +15,11 @@ public extension View {
     func infiniteWidth(maxHeight: CGFloat) -> some View {
         self.frame(maxWidth: .infinity, maxHeight: maxHeight)
     }
+
+
+    /// Positions this view within an invisible frame with infinite width and a specified alignment.
+    /// - Parameter alignment: The alignment of this view inside the resulting frame.
+    func infiniteWidth(alignment: Alignment = .center) -> some View {
+        self.frame(maxWidth: .infinity, alignment: alignment)
+    }
 }
